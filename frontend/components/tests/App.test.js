4f61673e195dda12f976ab61e6c6bde7bb5820dc
Matt Glenn <highopenended@gmail.com>
2024-10-03 12:15:36 -0400

    Completed Challenge

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -12,14 +12,16 @@ describe('Stranger Things App', () => {
   beforeAll(() => { server.listen() })
   afterAll(() => { server.close() })
   beforeEach(() => {
-    render(<App />)
+    render(<App />) // Mounts component to fake DOM
     user = userEvent.setup()
   })
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
+    screen.debug()
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
+    await user.click(screen.getByText('Press to Get Show Data'))
 
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
@@ -27,11 +29,18 @@ describe('Stranger Things App', () => {
     //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
     //    - The text "Select A Season" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
+    await waitFor(()=>{
+      expect(screen.queryByText('Press to Get Show Data')).not.toBeInTheDocument()
+      screen.getByText('Stranger Things')
+      screen.getByText("A love letter to the '80s classics that captivated a generation", { exact: false })
+      screen.getByText("Select A Season")
+    })
 
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
+    await user.selectOptions(document.querySelector('select'),'1')
 
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
@@ -39,5 +48,8 @@ describe('Stranger Things App', () => {
     //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
 
+    screen.getByText('Season 2, Episode 1')
+    screen.getByText('Chapter One: MADMAX')
+    screen.getByText('One year after the events with the Upside Down and the Demogorgon', { exact: false })
   })
 })
